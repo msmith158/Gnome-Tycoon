861b3f2b8e4c05d22e60461b8bc5d9be6a422578
msmith158 <1026618@student.sae.edu.au>
2024-07-29 23:32:36 +0800

    Final machine systems is done, needs implementation

--- Assets/Scripts/FinalDispenser.cs
+++ Assets/Scripts/FinalDispenser.cs
@@ -1,20 +1,73 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FinalDispenser : MonoBehaviour
 {
-    public GameObject newDispenserObject;
+    [SerializeField] private GameObject spawnTrigger;
+    [SerializeField] private GameObject newPrefab;
+    [HideInInspector] public PrototypeGnomeCoinSystem gnomeCoinSys;
+    public float manufacturingTime;
+    public float manufacturingCooldown;
+    [HideInInspector] public float initialManuTime;
+    [HideInInspector] public float initialManuCool;
+    public GameObject timeSlider;
+    public List<GameObject> objectsList = new List<GameObject>();
+    private bool isActivated = false;
+    private Scrollbar slider;
 
-    // Start is called before the first frame update
-    void Start()
+    void OnEnable()
     {
+        slider = timeSlider.GetComponent<Scrollbar>();
+        initialManuTime = manufacturingTime;
         
+        // Make sure to load the game via the loading level, otherwise these objects won't exist
+        gnomeCoinSys = GameObject.Find("proto_ddolManager").GetComponent<PrototypeGnomeCoinSystem>();
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    public void SpawnObject() // This function is to be called if trying to use the DelayedSpawn method
     {
-        
+        if (!isActivated)
+        {
+            isActivated = true;
+            StartCoroutine(DelayedSpawn());
+        }
+    }
+    
+    private IEnumerator DelayedSpawn()
+    {
+        // Work the manufacturing delay timer
+        timeSlider.SetActive(true);
+        timeSlider.transform.Find("timerText").GetComponent<TextMeshProUGUI>().text = "Manufacturing...";
+        manufacturingTime += (manufacturingTime * gnomeCoinSys.permanentTime);
+        manufacturingCooldown += (manufacturingCooldown * gnomeCoinSys.permanentCooldown);
+
+        for (float timer = manufacturingTime; timer > 0; timer -= Time.deltaTime)
+        {
+            timer = Mathf.Clamp(timer, 0f, manufacturingTime);
+            float progress = Mathf.InverseLerp(0f, manufacturingTime, timer);
+            slider.size = progress;
+            yield return null;
+        }
+
+        // Instantiate the new raw materials from the dispenser
+        GameObject newObject = Instantiate(newPrefab, spawnTrigger.transform.position, Quaternion.identity);
+        newObject.tag = "gnome";
+        objectsList.Add(newObject);
+
+        // Work the manufacturer cooldown timer
+        timeSlider.transform.Find("timerText").GetComponent<TextMeshProUGUI>().text = "Cooling down...";
+        for (float timer = 0; timer < manufacturingCooldown; timer += Time.deltaTime)
+        {
+            timer = Mathf.Clamp(timer, 0f, manufacturingCooldown);
+            float progress = Mathf.InverseLerp(0f, manufacturingCooldown, timer);
+            slider.size = progress;
+            yield return null;
+        }
+
+        timeSlider.SetActive(false);
+        isActivated = false;
     }
 }
--- Assets/Scripts/FinalMachineSystems.cs
+++ Assets/Scripts/FinalMachineSystems.cs
@@ -1,61 +1,128 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
-[RequireComponent(typeof(BoxCollider))]
 public class FinalMachineSystems : MonoBehaviour
 {
+    [Tooltip("It's ideal that you set these regardless of machine type.")][Header("General/Shared Properties")]
     [SerializeField] private MachineType machineType;
     [SerializeField] private GameObject spawnTrigger;
     [SerializeField] private GameObject newPrefab;
-    [SerializeField] private PrototypeManufacturer manufacturer;
-    private bool isActivated = false;
+    [SerializeField] private FinalDispenser dispenser;
+    [HideInInspector] public PrototypeDDOLManager initSys;
+    
+    [Header("Painter Properties")]
+    public PrototypeFactorySystem sys;
+    public List<Material> gnomeMaterialList = new List<Material>();
+    
+    [Header("Packager Properties")]
+    [SerializeField] private GameObject jimboPrefab;
+    [Tooltip("Put the max odds in whole numbers. For example, for a 1 in 10,000 chance, put 10000. This number CANNOT be 0.")] [SerializeField] private int chanceOfJimbo;
+    private int jimboNumber = 0;
+    private bool wasJimboSpawned;
+    
+    private void OnEnable()
+    {
+        // Make sure to load the game via the loading level, otherwise these objects won't exist
+        initSys = GameObject.Find("proto_ddolManager").GetComponent<PrototypeDDOLManager>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("gnome"))
+        if (other.CompareTag("gnome")) // Only call the function for the manufactured objects to stop accidentally processing other objects
         {
             switch (machineType)
             {
-                case MachineType.Sprayer:
+                case MachineType.Sprayer: // The functions for the sprayer machine
                     Destroy(other);
                     GameObject newSprayedObject = Instantiate(newPrefab, spawnTrigger.transform.position, Quaternion.identity);
                     newSprayedObject.tag = "gnome";
-                    manufacturer.objectsList.Add(newSprayedObject);
+                    dispenser.objectsList.Add(newSprayedObject);
                     break;
-                case MachineType.Moulder:
+                case MachineType.Moulder: // The functions for the moulder machine
                     Destroy(other);
-                    GameObject newObject = Instantiate(newPrefab, spawnTrigger.transform.position, Quaternion.identity);
-                    newObject.tag = "gnome";
-                    manufacturer.objectsList.Add(newObject);
+                    GameObject newMouldedObject = Instantiate(newPrefab, spawnTrigger.transform.position, Quaternion.identity);
+                    newMouldedObject.tag = "gnome";
+                    dispenser.objectsList.Add(newMouldedObject);
+                    break;
+                case MachineType.Painter: // The functions for the painter machine
+                    // Apply the prestige-relevant material to the newly-spawned gnome
+                    if (initSys.resetTimes == 0)
+                    {
+                        switch (sys.prestigeLvl)
+                        {
+                            case PrototypeFactorySystem.PrestigeLevel.Prestige0: // Dirty red gnome material
+                                for (int i = 0; i < other.transform.childCount; i++)
+                                {
+                                    other.transform.GetChild(i).GetComponent<Renderer>().material = gnomeMaterialList[0];
+                                }
+                                break;
+                            case PrototypeFactorySystem.PrestigeLevel.Prestige1: // Red gnome material
+                                for (int i = 0; i < other.transform.childCount; i++)
+                                {
+                                    other.transform.GetChild(i).GetComponent<Renderer>().material = gnomeMaterialList[1];
+                                }
+                                break;
+                            case PrototypeFactorySystem.PrestigeLevel.Prestige2: // Yellow gnome material
+                                for (int i = 0; i < other.transform.childCount; i++)
+                                {
+                                    other.transform.GetChild(i).GetComponent<Renderer>().material = gnomeMaterialList[2];
+                                }
+                                break;
+                            case PrototypeFactorySystem.PrestigeLevel.Prestige3: // Green gnome material
+                                for (int i = 0; i < other.transform.childCount; i++)
+                                {
+                                    other.transform.GetChild(i).GetComponent<Renderer>().material = gnomeMaterialList[3];
+                                }
+                                break;
+                            case PrototypeFactorySystem.PrestigeLevel.Prestige4: // Cyan gnome material
+                                for (int i = 0; i < other.transform.childCount; i++)
+                                {
+                                    other.transform.GetChild(i).GetComponent<Renderer>().material = gnomeMaterialList[4];
+                                }
+                                break;
+                            case PrototypeFactorySystem.PrestigeLevel.Prestige5: // Purple gnome material
+                                for (int i = 0; i < other.transform.childCount; i++)
+                                {
+                                    other.transform.GetChild(i).GetComponent<Renderer>().material = gnomeMaterialList[5];
+                                }
+                                break;
+                        }
+                    }
+                    else if (initSys.resetTimes >= 1) // If the game has been reset at least once, golden gnome material
+                    {
+                        for (int i = 0; i < other.transform.childCount; i++)
+                        {
+                            other.transform.GetChild(i).GetComponent<Renderer>().material = gnomeMaterialList[6];
+                        }
+                    }
+                    break;
+                case MachineType.Packager:
+                    Destroy(other);
+                    // Do a roll between a gnome and Jimbo, and then instantiate
+                    jimboNumber = Random.Range(0, chanceOfJimbo);
+                    Debug.Log(jimboNumber);
+                    if (jimboNumber == 0 && sys.prestigeLvl != PrototypeFactorySystem.PrestigeLevel.Prestige0) // This is to spawn Jimbo
+                    {
+                        GameObject newJimbo = Instantiate(jimboPrefab, spawnTrigger.transform.position, Quaternion.identity);
+                        newJimbo.tag = "gnome";
+                        dispenser.objectsList.Add(newJimbo);
+                    }
+                    else // This is to spawn the raw materials out of the dispenser
+                    {
+                        GameObject newPackagedObject = Instantiate(newPrefab, spawnTrigger.transform.position, Quaternion.identity);
+                        newPackagedObject.tag = "gnome";
+                        dispenser.objectsList.Add(newPackagedObject);
+                    }
                     break;
             }
-            /* COPY + PASTE BELOW
-            Destroy(other);
-            GameObject newObject = Instantiate(newPrefab, spawnTrigger.transform.position, Quaternion.identity);
-            newObject.tag = "gnome";
-            manufacturer.objectsList.Add(newObject);
-            */
         }
     }
 
-    public void SpawnObject()
+    private enum MachineType // YOU MUST SET YOUR MACHINES TO ONE OF THESE IN ORDER TO MAKE THIS SCRIPT WORK!
     {
-        if (!isActivated)
-        {
-            isActivated = true;
-            StartCoroutine(DelayedSpawn());
-        }
-    }
-
-    private IEnumerator DelayedSpawn()
-    {
-        yield return null;
-    }
-
-    private enum MachineType
-    {
-        Dispenser,
         Sprayer,
         Moulder,
         Painter,
