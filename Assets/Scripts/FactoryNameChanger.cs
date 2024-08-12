32463e997d35928b4486eed97614aeb71c6a601e
msmith158 <1026618@student.sae.edu.au>
2024-08-12 19:21:52 +0800

    Factory naming mechanic is implemented

--- Assets/Scripts/FactoryNameChanger.cs
+++ Assets/Scripts/FactoryNameChanger.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
+// Assign this script to the Factory Name Text object.
 public class FactoryNameChanger : MonoBehaviour
 {
-    [SerializeField] private MainMenuScript menuSys;
+    private MainMenuScript menuSys;
     
     void OnEnable()
     {
-        
+        menuSys = GameObject.Find("mainMenuSystemHandler").GetComponent<MainMenuScript>();
+        GetComponent<TextMeshPro>().text = "Mom & Pop's\n" + menuSys.adjective + " Gnomes";
     }
 }
